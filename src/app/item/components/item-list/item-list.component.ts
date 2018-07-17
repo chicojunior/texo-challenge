f55f4621f6d558f98176c1b2679d6ef4e9aa62f6
Francisco do Vale <francisco.vale@iviatecnologia.com.br>
2018-07-17 20:13:33 -0300

    Preparação para push no github

--- src/app/item/components/item-list/item-list.component.ts
+++ src/app/item/components/item-list/item-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Item } from '../../shared/item';
+
+
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemListComponent implements OnInit {
 
+  itens: Item[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    const arroz = new Item();
+    arroz.nomeItem = 'Arroz';
+    arroz.perecivel = false;
+    arroz.preco = 3.75;
+    arroz.dataFabricacao = new Date(2018, (7 - 1), 15);
+
+    const feijao = new Item();
+    feijao.nomeItem = 'Feijão';
+    feijao.perecivel = false;
+    feijao.preco = 5.56;
+    feijao.dataFabricacao = new Date(2018, (7 - 1), 15);
+
+    this.itens.push(arroz);
+    this.itens.push(feijao);
   }
 
 }
--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ToolbarModule } from 'primeng/toolbar';
+import { SidebarModule } from 'primeng/sidebar';
+import { DataViewModule } from 'primeng/dataview';
+
 import { ItemRoutingModule } from './item-routing.module';
+
 import { ItemComponent } from './item.component';
 import { ItemAddComponent } from './components/item-add/item-add.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
@@ -9,7 +14,10 @@ import { ItemListComponent } from './components/item-list/item-list.component';
 @NgModule({
   imports: [
     CommonModule,
-    ItemRoutingModule
+    ItemRoutingModule,
+    ToolbarModule,
+    SidebarModule,
+    DataViewModule
   ],
   declarations: [
     ItemComponent,
